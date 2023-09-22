ffacf25e15391f4d959ba1ecea5d09f687a6b85c
Abhilash Reddy <abhilashreddy67@gmail.com>
2023-09-22 17:26:17 +0530

    first branch commit

--- src/main/java/com/example/gitbranches/HelloApplication.java
+++ src/main/java/com/example/gitbranches/HelloApplication.java
@@ -9,4 +9,14 @@ public class HelloApplication extends Application {
     public String toString() {
         return super.toString();
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
