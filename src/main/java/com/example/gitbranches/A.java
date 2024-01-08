147323e9db6293860e0332a5fe207969e4df44c5
Abhilash Reddy <abhilashreddy67@gmail.com>
2024-01-08 17:18:19 +0530

    First commit

---
+++ src/main/java/com/example/gitbranches/A.java
@@ -0,0 +1,9 @@
+package com.example.gitbranches;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
