224cef5f18f89b526530ba2555bdcaca5f4d8446
Abhilash Reddy <abhilashreddy67@gmail.com>
2024-01-08 17:27:29 +0530

    changes in branch1

--- src/main/java/com/example/gitbranches/A.java
+++ src/main/java/com/example/gitbranches/A.java
@@ -5,5 +5,6 @@ import org.w3c.dom.ls.LSOutput;
 public class A {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Changes made to branch1");
     }
 }
