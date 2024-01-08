33f3510c3886433367fe03322a1db93499bace5c
Abhilash Reddy <abhilashreddy67@gmail.com>
2024-01-08 17:36:11 +0530

    some more changes to master

--- src/main/java/com/example/gitbranches/A.java
+++ src/main/java/com/example/gitbranches/A.java
@@ -7,5 +7,6 @@ public class A {
 
         System.out.println("Hello World");
         System.out.println("Changes made to branch1 and master here");
+        System.out.println("Another change to master to create Merge-Conflict");
     }
 }
